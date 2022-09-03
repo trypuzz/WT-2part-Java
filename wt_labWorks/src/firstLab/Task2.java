5ce81a4974757f337eafe886f08319604eac4377
trypuzz <iv_tripuz@mail.ru>
2022-09-03 09:18:54 +0300

    2 task + finished 4th task

---
+++ wt_labWorks/src/firstLab/Task2.java
@@ -0,0 +1,28 @@
+package firstLab;
+
+import java.util.Scanner;
+
+public class Task2 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter scale: ");
+		int scale = scanner.nextInt();
+
+		System.out.print("Enter X of the point: ");
+		int pointX = scanner.nextInt();
+		System.out.print("Enter Y of the point: ");
+		int pointY = scanner.nextInt();
+
+		boolean flag = false;
+		if ((pointX >= -4 * scale && pointX <= 4 * scale) && (pointY <= 5 * scale && pointY >= 0)) {
+			flag = true;
+			System.out.println("Point is in this area (flag = " + flag + ")");
+		} else if ((pointX >= -6 * scale && pointX <= 6 * scale) && (pointY >= 3 * scale && pointY <= 0)){
+			flag = true;
+			System.out.println("Point is in this area (flag = " + flag + ")");
+		} else {
+			flag = false;
+			System.out.println("Point is not in this area (flag = " + flag + ")");
+		}
+	}
+}
--- wt_labWorks/src/firstLab/Task4.java
+++ wt_labWorks/src/firstLab/Task4.java
@@ -1,21 +1,30 @@
 package firstLab;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Task4 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		Random random = new Random();
+
 		int n = scanner.nextInt();
 		int[] myArray = new int[n];
 
+		for (int i = 0; i < n; i++) {
+			myArray[i]= random.nextInt(100);
+			System.out.print(myArray[i] + ", ");
+		}
+		System.out.println();
+
 		for (int i = 0; i < myArray.length; i++) {
-			int count = 0;
-			for (int j = 0; j < myArray[i]; j++) {
-				if (myArray[i] % j != 0) {
-					count++;
+			boolean flag = true;
+			for (int j = 2; j < myArray[i]; j++) {
+				if (myArray[i] % j == 0) {
+					flag = false;
 				}
 			}
-			if (count == 0) {
+			if (flag == true) {
 				System.out.println(i + ") " + myArray[i]);
 			}
 		}
